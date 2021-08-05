05e6aba92f97d3a5c951ad2e56728b48a1749808
Vinit <vinit@Vinits-MacBook-Air.local>
2021-08-05 09:54:54 +0530

    Factorial issue fix

--- Factorial Program.py
+++ Factorial Program.py
@@ -1,12 +1,11 @@
 def fact(n):
-    f = 1
+    fac = 1
     for i in range(1,n+1):
-        f = f * i
-    return f
+        fac = fac * i
+    return fac
 
 x = int(input("Enter Your Number : "))
 
 result = fact(x)
-
 
 print(result)
