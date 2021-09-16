2c722f76eda753b5994393ebae776920a0ea052f
Vinit Kumar Vijal <vineet.shanker@soesector17rohini.co.in>
2021-09-16 09:11:23 +0530

    if Else Used

--- Factorial Program.py
+++ Factorial Program.py
@@ -1,7 +1,6 @@
 # WAP to input any number from user and calculate factorial of a number.
 # Created By Vinit
 
-
 def fact(n):
     fac = 1
     for i in range(1,n+1):
@@ -9,7 +8,8 @@ def fact(n):
     return fac
 
 x = int(input("Enter Your Number : "))
-
-result = fact(x)
-
-print(result)
+if x >= 1:
+    result = fact(x)
+    print(result)
+else:
+    print("Your Input is (-) Negative!!!")
